a96110693e4107d2250a7dd777987dd3f3aa7ec3
Achraftraf <achrafzarouki20@gmail.com>
2024-10-13 14:13:25 +0100

    i created the navbar for the project to navigate for each page

--- app/components/SidebarHeader.jsx
+++ app/components/SidebarHeader.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import ThemeToggle from "./ThemeToggle";
+import { RiOpenaiLine } from "react-icons/ri";
 
 const SidebarHeader = () => {
-  return <div>SidebarHeader</div>;
+  return (
+    <div className="flex items-center mb-4 gap-4 px-4">
+      <RiOpenaiLine className="w-10 h-10 text-primary" />
+      <h2 className="text-xl font-extrabold text-primary mr-auto">GeniusAI</h2>
+      <ThemeToggle />
+    </div>
+  );
 };
-
 export default SidebarHeader;
